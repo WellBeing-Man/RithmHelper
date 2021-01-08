45d55335581d74d94547fef73b57d0040824ac80
KR620574 <63383119+KR620574@users.noreply.github.com>
2021-01-08 13:50:29 +0900

    maze todo
    maze todo

---
+++ src/main/java/recursion/Maze.java
@@ -0,0 +1,50 @@
+package recursion;
+
+
+
+public class Maze {
+
+    private int[][] map;
+    private int currentPositionX, currentPositionY;
+
+    public Maze(int[][] n) {
+        map=n;
+    }
+
+    //Chaining method
+    Maze setCurrentPosition(int currentPositionX,int currentPositionY){
+        this.currentPositionX=currentPositionX;
+        this.currentPositionY=currentPositionY;
+        return this;
+    }
+
+
+    boolean findWay(int x, int y) {
+
+        return true;
+    }
+
+    void printMap(){
+        for(int[] line : map){
+            for(int row : line){
+                System.out.print(row + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private enum PATH{
+        WAY(0),WALL(1),VISITED(3),BLOCKED(4);
+
+        final private int tag;
+
+        public int getTag() {
+            return tag;
+        }
+
+        PATH(int s) {
+
+            tag=s;
+        }
+    }
+}
