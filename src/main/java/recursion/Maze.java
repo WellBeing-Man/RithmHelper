2aeb8b617f88ee2ff00288ecaec389a98f234325
WellBeing-Man <leesrock113@gmail.com>
2021-01-11 06:31:25 +0900

    maze done

--- src/main/java/recursion/Maze.java
+++ src/main/java/recursion/Maze.java
@@ -6,45 +6,76 @@ public class Maze {
 
     private int[][] map;
     private int currentPositionX, currentPositionY;
+    private int max;
 
     public Maze(int[][] n) {
         map=n;
+        max =n.length;
     }
 
     //Chaining method
-    Maze setCurrentPosition(int currentPositionX,int currentPositionY){
+    public Maze setCurrentPosition(int currentPositionX,int currentPositionY){
         this.currentPositionX=currentPositionX;
         this.currentPositionY=currentPositionY;
         return this;
     }
 
 
-    boolean findWay(int x, int y) {
-        //todo recursive maze
-        return true;
+    private boolean findWay(int x, int y) {
+
+        if(x<0 || y<0 || x>= max || y>= max)
+            return false;
+        else if(map[x][y] == PATH.EXIT.TAG) {
+            return true;
+        }else if(map[x][y] != PATH.WAY.TAG){
+            return false;
+        }else{
+            map[x][y] = PATH.VISITED.TAG;
+            if(findWay(x+1,y) || findWay(x-1,y) || findWay(x,y+1)|| findWay(x,y-1)) {
+                return true;
+            }
+            map[x][y]=PATH.BLOCKED.TAG;
+            return false;
+        }
     }
 
-    void printMap(){
+    private void printMap(){
         for(int[] line : map){
             for(int row : line){
                 System.out.print(row + " ");
             }
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println();
     }
 
-    private enum PATH{
-        WAY(0),WALL(1),VISITED(3),BLOCKED(4);
 
-        final private int tag;
+
+    public boolean explore(){
+        boolean result;
+        printMap();
+        result=findWay(currentPositionX,currentPositionY);
+        printMap();
+
+
+        return result;
+    }
+
+
+    private enum PATH{
+        WAY(0),WALL(1),VISITED(2),BLOCKED(3),EXIT(4);
+
+        final private int TAG;
 
         public int getTag() {
-            return tag;
+            return TAG;
         }
 
         PATH(int s) {
 
-            tag=s;
+            TAG =s;
         }
     }
 }
