cc6e9df9fd1ac826a6b55b8716528ef23485ac5a
WellBeing-Man <leesrock113@gmail.com>
2021-01-19 17:47:22 +0900

    Quick, Heap Sort TODO

---
+++ src/main/java/sorting/HeapSort.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public class HeapSort extends Sort {
+    public HeapSort(int[] array) {
+        super(array);
+    }
+
+    @Override
+    public int[] sort() {
+        //todo Heap sort
+        return new int[0];
+    }
+}
---
+++ src/main/java/sorting/QuickSort.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public class QuickSort extends Sort {
+    public QuickSort(int[] array) {
+        super(array);
+    }
+
+    @Override
+    public int[] sort() {
+        //TODO Quick Sort
+        return new int[0];
+    }
+}
