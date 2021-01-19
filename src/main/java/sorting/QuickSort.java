5ad7c85748ccc0b64b644b3c55067be0c440c21f
WellBeing-Man <leesrock113@gmail.com>
2021-01-19 20:01:23 +0900

    QuickSort Done

--- src/main/java/sorting/QuickSort.java
+++ src/main/java/sorting/QuickSort.java
@@ -7,7 +7,32 @@ public class QuickSort extends Sort {
 
     @Override
     public int[] sort() {
-        //TODO Quick Sort
-        return new int[0];
+        quickSort(array,0,max-1);
+        return array;
+
+    }
+
+    private void quickSort(int[] array,int p, int r) {
+        if(p<r){
+            int q= partition(array,p,r);
+            quickSort(array,p,q-1);
+            quickSort(array,q+1,r);
+        }
+    }
+
+    private int partition(int[] array, int p, int r) {
+
+        int pivot=array[r];
+        int i=p-1;
+        int j=p;
+        while (j<r){
+            if(array[j]<pivot) {
+                i++;
+                swap(array,i,j);
+            }
+            j++;
+        }
+        swap(array,i+1,r);
+        return i+1;
     }
 }
--- src/test/java/SortingTest.java
+++ src/test/java/SortingTest.java
@@ -1,11 +1,18 @@
 import org.junit.Test;
 import sorting.*;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class SortingTest {
     @Test
     public void SortingTest() {
+
+        ArrayList<int[]> input=new ArrayList<>();
+        ArrayList<int[]> output=new ArrayList<>();
+
+
         int[] beforeCase={5,1,2,4,3};
         int[] afterCase={1,2,3,4,5};
 
@@ -18,28 +25,23 @@ public class SortingTest {
         int[] beforeCase3={33,1,0,150,350,11,2,17,5,7};
         int[] afterCase3= {0,1,2,5,7,11,17,33,150,350};
 
+        input.add(beforeCase);
+        input.add(beforeCase1);
+        input.add(beforeCase2);
+        input.add(beforeCase3);
 
+        output.add(afterCase);
+        output.add(afterCase1);
+        output.add(afterCase2);
+        output.add(afterCase3);
 
-
-        assertEquals(arrayToString(new SelectionSort(beforeCase).sort()),arrayToString(afterCase));
-        assertEquals(arrayToString(new BubbleSort(beforeCase).sort()),arrayToString(afterCase));
-        assertEquals(arrayToString(new InsertionSort(beforeCase).sort()),arrayToString(afterCase));
-        assertEquals(arrayToString(new MergeSort(beforeCase).sort()),arrayToString(afterCase));
-
-        assertEquals(arrayToString(new SelectionSort(beforeCase1).sort()),arrayToString(afterCase1));
-        assertEquals(arrayToString(new BubbleSort(beforeCase1).sort()),arrayToString(afterCase1));
-        assertEquals(arrayToString(new InsertionSort(beforeCase1).sort()),arrayToString(afterCase1));
-        assertEquals(arrayToString(new MergeSort(beforeCase1).sort()),arrayToString(afterCase1));
-
-        assertEquals(arrayToString(new SelectionSort(beforeCase2).sort()),arrayToString(afterCase2));
-        assertEquals(arrayToString(new BubbleSort(beforeCase2).sort()),arrayToString(afterCase2));
-        assertEquals(arrayToString(new InsertionSort(beforeCase2).sort()),arrayToString(afterCase2));
-        assertEquals(arrayToString(new MergeSort(beforeCase2).sort()),arrayToString(afterCase2));
-
-        assertEquals(arrayToString(new SelectionSort(beforeCase3).sort()),arrayToString(afterCase3));
-        assertEquals(arrayToString(new BubbleSort(beforeCase3).sort()),arrayToString(afterCase3));
-        assertEquals(arrayToString(new InsertionSort(beforeCase3).sort()),arrayToString(afterCase3));
-        assertEquals(arrayToString(new MergeSort(beforeCase3).sort()),arrayToString(afterCase3));
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(arrayToString(new SelectionSort(input.get(i)).sort()),arrayToString(output.get(i)));
+            assertEquals(arrayToString(new BubbleSort(input.get(i)).sort()),arrayToString(output.get(i)));
+            assertEquals(arrayToString(new InsertionSort(input.get(i)).sort()),arrayToString(output.get(i)));
+            assertEquals(arrayToString(new MergeSort(input.get(i)).sort()),arrayToString(output.get(i)));
+            assertEquals(arrayToString(new QuickSort(input.get(i)).sort()),arrayToString(output.get(i)));
+        }
 
     }
 
