1c21ab3c0a2d4043a57c083ea9ad672367364f60
WellBeing-Man <leesrock113@gmail.com>
2021-01-27 20:43:17 +0900

    BST progressing

--- src/main/java/search_tree/BinarySearchTree.java
+++ src/main/java/search_tree/BinarySearchTree.java
@@ -6,23 +6,87 @@ public class BinarySearchTree extends Tree<BinaryTreeNode> {
 
     public BinarySearchTree(int rootData) {
         super(rootData);
-        root=new BinaryTreeNode(rootData);
-        size++;
     }
 
 
     @Override
     public TreeNode search(int input) {
-        return null;
+        BinaryTreeNode node=(BinaryTreeNode) root;
+
+        while (node.getData()!=input){
+            if(node.getData()>input){
+                node=node.getRight();
+            }else {
+                node=node.getLeft();
+            }
+        }
+
+        if(node.getData()!=input){
+            return null;
+        }
+        return node;
     }
 
     @Override
     public TreeNode insert(int T) {
-        return null;
+
+
+        BinaryTreeNode node=(BinaryTreeNode) root;
+        BinaryTreeNode newNode= new BinaryTreeNode(T);
+        BinaryTreeNode parent=null;
+        if(root==null){
+            root=newNode;
+        }
+
+        while (node!=null){
+            parent=node;
+            if(node.getData()>T){
+                    node=node.getRight();
+            }else {
+                node=node.getLeft();
+            }
+        }
+
+        if(newNode.getData()>parent.getData()){
+            parent.setRight(newNode);
+        }else{
+            parent.setLeft(newNode);
+        }
+
+        newNode.parent=parent;
+
+        return newNode;
     }
 
     @Override
     public boolean delete(int input) {
         return false;
+    }
+
+
+
+    private BinaryTreeNode findSuccessor(BinaryTreeNode node){
+        if (node.getRight()!=null){
+            return smallestCase1(node.getRight());
+        }else {
+            return  rightChildCase2(node);
+        }
+    }
+
+    private BinaryTreeNode smallestCase1(BinaryTreeNode node) {
+            if(node.getLeft() == null){
+                return node;
+            }
+            return smallestCase1(node.getLeft());
+    }
+
+    private BinaryTreeNode rightChildCase2(BinaryTreeNode node){
+        BinaryTreeNode parent=node.parent;
+        while (parent.getLeft() != node){
+            node=parent;
+            parent=node.getParent();
+        }
+
+        return node.getRight();
     }
 }
